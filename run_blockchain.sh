572c2dec7754b79b2708dece07e8fafdda91c93a
chihong <c.hong@tudelft.nl>
2024-01-23 13:54:47 +0100

    .

--- run_blockchain.sh
+++ run_blockchain.sh
@@ -1,5 +1,5 @@
 
-NUM_NODES=5
+NUM_NODES=10
 python src/util.py $NUM_NODES topologies/blockchain.yaml blockchain
 docker compose build
 docker compose up
--- run_election.sh
+++ run_election.sh
@@ -1,5 +1,5 @@
 
-NUM_NODES=4
+NUM_NODES=10
 python src/util.py $NUM_NODES topologies/election.yaml election
 docker compose build
 docker compose up
